1129d48900e548b08c3a8b9830acadc9e57216fc
Raed Romanov <raid_r@mail.ru>
2020-04-08 20:17:18 +0300

    Remove unused class

--- src/commonMain/kotlin/crdt/seq/SequenceElement.kt
+++
@@ -1,9 +0,0 @@
-package raid.neuroide.reproto.crdt.seq
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SequenceElement(val pid: Identifier, val content: String) : Comparable<SequenceElement> {
-    override fun compareTo(other: SequenceElement) =
-        compareValuesBy(this, other, SequenceElement::pid, SequenceElement::content)
-}
